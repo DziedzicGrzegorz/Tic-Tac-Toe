7a0ad99fd51acc4333086104ea0dedbaf4a272a3
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2024-05-31 08:25:39 +0200

    fix: 🚑(constants): lost file

---
+++ constants/main.go
@@ -0,0 +1,24 @@
+package constants
+
+const (
+	Empty     = 0
+	PlayerX   = 1
+	PlayerO   = -1
+	BoardSize = 3
+	Down      = "down"
+	Left      = "left"
+	Right     = "right"
+	Quit      = "q"
+	Esc       = "esc"
+	M         = "m"
+	Tab       = "tab"
+	ShiftTab  = "shift+tab"
+	Enter     = "enter"
+	Up        = "up"
+	DownKey   = "down"
+	LeftKey   = "left"
+	RightKey  = "right"
+	EscKey    = "esc"
+	CtrlC     = "ctrl+c"
+	CtrlR     = "ctrl+r"
+)
